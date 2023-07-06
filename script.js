1514aad6bbd354791424b03bbbdf7eb03d297b92
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-06 06:53:21 +0200

    Add basic structure of script

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let myLibrary = [];
+
+function Book(author, title, numPages, haveRead) {
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.haveRead = function () {
+    return true;
+  };
+}
+
+const al = new Book("Bayanda", "Alch", 234, true);
+
+function storeBookObj(obj) {
+  myLibrary.push(obj);
+}
+
+storeBookObj.call(Book, al);
+
+const displayLibrary = (myLibrary) => {};
