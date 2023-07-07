cdb67ec75a168eed3c56164e1045ce313f643c96
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-07 11:19:00 +0200

    Add functionality to add book button

--- script.js
+++ script.js
@@ -3,6 +3,33 @@
 // SELECT ELEMENTS
 
 const addBookBtn = document.querySelector(".add-book");
+const footer = document.querySelector(".signature");
+const overlay = document.querySelector(".overlay");
+const formModal = document.querySelector(".form");
+const formCore = document.querySelector(".core-form");
+const exitBtn = document.querySelector(".exit");
+
+// DESIGN:
+
+// MODAL :
+addBookBtn.addEventListener("click", () => {
+  // Add
+
+  addBookBtn.classList.add("hidden");
+  footer.classList.add("hidden");
+
+  // Remove
+
+  exitBtn.classList.remove("hidden");
+  formCore.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  formModal.classList.remove("hidden");
+});
+
+console.log(exitBtn);
+
+// Functionality:
+
 let myLibrary = [];
 
 function Book(author, title, numPages, haveRead) {
