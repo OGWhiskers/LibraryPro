df879dab5edc3356d8ec309419ecd76a118ea122
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-07 16:01:15 +0200

    Add final touches with styling web page

--- script.js
+++ script.js
@@ -2,32 +2,73 @@
 
 // SELECT ELEMENTS
 
+// OTHERS:
+
 const addBookBtn = document.querySelector(".add-book");
 const footer = document.querySelector(".signature");
 const overlay = document.querySelector(".overlay");
+
+// FORM ELEMENTS
+
 const formModal = document.querySelector(".form");
 const formCore = document.querySelector(".core-form");
 const exitBtn = document.querySelector(".exit");
+const addForm = document.querySelector(".sub-form");
+
+// FORM DATA:
+
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+
+let authorForm = "";
+let titleForm = "";
+let pagesForm = 0;
+let readForm = true;
 
 // DESIGN:
 
+const toggleHidden = () => {
+  let arr = [addBookBtn, footer, exitBtn, formCore, overlay, formModal];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].classList.contains("hidden")
+      ? arr[i].classList.remove("hidden")
+      : arr[i].classList.add("hidden");
+  }
+};
+
+const escapeModal = (e) =>
+  e.key === "Escape" && addBookBtn.classList.contains("hidden")
+    ? toggleHidden()
+    : false;
+
 // MODAL :
-addBookBtn.addEventListener("click", () => {
-  // Add
 
-  addBookBtn.classList.add("hidden");
-  footer.classList.add("hidden");
+addBookBtn.addEventListener("click", toggleHidden);
+exitBtn.addEventListener("click", toggleHidden);
+overlay.addEventListener("click", toggleHidden);
+document.addEventListener("keyup", escapeModal);
 
-  // Remove
+// FORM SUBMITION:
 
-  exitBtn.classList.remove("hidden");
-  formCore.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-  formModal.classList.remove("hidden");
+formCore.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  authorForm = author.value;
+  titleForm = title.value;
+  pagesForm = pages.value;
+  readForm = read.value;
+
+  const al = new Book(authorForm, titleForm, pagesForm, readForm);
+
+  console.log(al);
+
+  storeBookObj.call(Book, al);
+
+  console.log(myLibrary);
 });
-
-console.log(exitBtn);
-
 // Functionality:
 
 let myLibrary = [];
@@ -41,12 +82,16 @@ function Book(author, title, numPages, haveRead) {
   };
 }
 
-const al = new Book("Bayanda", "Alch", 234, true);
+const al = new Book(authorForm, titleForm, pagesForm, readForm);
+
+console.log(al);
 
 function storeBookObj(obj) {
   myLibrary.push(obj);
 }
 
 storeBookObj.call(Book, al);
+
+console.log(myLibrary);
 
 const displayLibrary = (myLibrary) => {};
